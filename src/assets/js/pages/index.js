0f8ee871c8d55decf9b22133e0c8bb1704232cb7
aisteron <timotheus@list.ru>
2024-05-27 23:07:42 +0300

    w27.05

--- src/assets/js/pages/index.js
+++ src/assets/js/pages/index.js
@@ -2,6 +2,8 @@ import { qs } from "../libs"
 
 export function Pages(){
 	//fullPage()
+	
+	fp()
 }
 
 async function fullPage(){
@@ -118,5 +120,35 @@ async function fullPage(){
 
 	//})();
 
+}
+
+function fp(){
+	
+	var divs = $('section');
+  var div = 0;
+  div = -1
+  divs.each(function(i) {
+    if (div < 0 && ($(this).offset().top >= $(window).scrollTop())) {
+        div = i;
+    }
+  });
+  $(window).on('mousewheel DOMMouseScroll', function(e) {
+    if (e.originalEvent.wheelDelta > 0 || e.originalEvent.detail < 0) {
+      if (div > 0) {
+        div--;
+      }
+    } else {
+      if (div < divs.length) {
+        div++;
+      }
+    }
+    $('html,body').stop().animate({
+      scrollTop: divs.eq(div).offset().top
+    }, 1000);
+    return false;
+  });
+  $(window).resize(function() {
+    $('html,body').scrollTop(divs.eq(div).offset().top);
+  });
 }
 
