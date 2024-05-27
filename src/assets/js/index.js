a7cf323efd0aa495f15730d580837cda514783c1
aisteron <timotheus@list.ru>
2024-05-27 22:48:22 +0300

    w27.05

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -1,9 +1,10 @@
 //import { Ui } from "./ui";
+import { Pages } from "./pages";
 
 document.readyState !== 'loading' ? init() : document.addEventListener('DOMContentLoaded', init);
 
 function init(){
 	//Ui()
-	//Pages()
-  console.log(0)
+	Pages()
+  
 }
